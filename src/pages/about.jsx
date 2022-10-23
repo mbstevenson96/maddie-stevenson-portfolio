92414d65f2af6e7e4d94d12b3f92d06e1c1eda54
mstevenson <mbstevenson96@gmail.com>
2022-10-23 19:03:10 -0400

    fix home page and add content to about me page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,15 @@
-
+import MAndA from '../assets/MAndA.jpg'
 
 const About = () => {
   return ( 
-    <h1>This is the About component</h1>
+    <>
+      <div>
+        <img src={MAndA} alt="Maddie and dog Ace on hike"/>
+      </div>
+      <h1>About Me</h1>
+      <p>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
+      <p>When I am not pursuing my passion of software engineering I am discovering new outdoor adventures, training with my dog Ace, reading, and exploring new travels.</p>
+    </>
   );
 }
 
