d971027b6e1e1fde36b256fe3a3b6c80bc2c188f
mstevenson <mbstevenson96@gmail.com>
2022-10-23 19:50:34 -0400

    fill in contact page with information

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ View the portfolio of software developer Maddie Stevenson here!
 
 ## Assets:
 - favicon image from Giphy user Highlands Students: https://giphy.com/stickers/best-motion-i4jGIjBa7kdKm3ockY
+- MailTo: feature from CodingYourLife user on StackOverflow: https://stackoverflow.com/questions/63782544/react-open-mailto-e-mail-client-onclick-with-body-from-textarea
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,28 @@
+import Gmail from '../assets/Gmail.png'
+import GitHub from '../assets/GitHub.png'
+import Linkedin from '../assets/Linkedin.png'
+import ButtonMailto from '../components/ButtonMailTo/ButtonMailTo'
 
 
 const Contact = () => {
   return ( 
-    <h1>This is the Contact component</h1>
+    <>
+      <h1>Contact Me!</h1>
+      <div>
+        <div>
+          <img src={Gmail} alt="gmail icon"/>
+          <ButtonMailto label="Email!" mailto="mailto:mbstevenson96@gmail.com" />
+        </div>
+        <div>
+          <img src={GitHub} alt="githib icon"/>
+          <a href='https://github.com/mbstevenson96' target="_blank" rel="noopener noreferrer">GitHub Profile</a>
+        </div>
+        <div>
+          <img src={Linkedin} alt="linkedin icon"/>
+          <a href='https://www.linkedin.com/in/mstevenson11/' target="_blank" rel="noopener noreferrer">Linkedin Profile</a>
+        </div>
+      </div>
+    </>
   );
 }
 
