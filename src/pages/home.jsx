b8f99406a8596279dcf07a9e95b43be16721c54d
mstevenson <mbstevenson96@gmail.com>
2022-10-23 18:55:04 -0400

    stub out home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,15 @@
-
+import Maddie from '../assets/Maddie.jpeg'
 
 const Home = () => {
   return ( 
-    <h1>This is the Home component</h1>
+    <>
+      <div>
+        <img src={Maddie} alt="Maddie in garden"/>
+      </div>
+      <h1>Maddie Stevenson | Software Engineer</h1>
+      <p>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
+      <p>When I am not pursuing my passion of software engineering I am discovering new outdoor adventures, training with my dog Ace, reading, and exploring new travels.</p>
+    </>
   );
 }
 
