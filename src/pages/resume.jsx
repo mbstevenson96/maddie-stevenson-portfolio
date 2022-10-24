882a2ccf0647cc617c7d2e5be1f3ef358d1b7056
mstevenson <mbstevenson96@gmail.com>
2022-10-23 20:18:20 -0400

    add content to resume page

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,8 +1,20 @@
+import MaddieLamb from '../assets/MaddieLamb.jpg'
+import ResumeIcon from '../assets/ResumeIcon.jpg'
 
 
 const Resume = () => {
   return ( 
-    <h1>This is the Resume component</h1>
+    <>
+      <div>
+        <img src={MaddieLamb} alt="Maddie hiking in Ireland"/>
+      </div>
+      <h1>Click the icon below to view my resume!</h1>
+      <a href='https://drive.google.com/file/d/1Nop0vBCSzTG6x-19dIvkNj3SPp_JJ20u/view?usp=sharing' target="_blank" rel="noopener noreferrer">
+        <button>
+          <img src={ResumeIcon} alt="resume icon"/>
+        </button>
+      </a>
+    </>
   );
 }
 
