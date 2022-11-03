3f96facd129dfe5eb1b3fb92f2c7b014aceea99c
mstevenson <mbstevenson96@gmail.com>
2022-11-03 08:52:24 -0400

    store project data in projects.js folder

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    image: 'https://i.imgur.com/9uj383C.png',
+    title: 'Adventures of Ace',
+    description: "Ace is a Bulldog American Staffordshire Terrier mix who loves to play ball. An issue has arose, though, where he has lost his ball and is sad because of this. He is looking for someone to assist him in searching and wants to know if that will be you? Work through this story game to find out what happened to Ace's ball and be his hero. Watch out for sad Ace, though, because one wrong move can cause great disappointment!",
+    repositoryLink: 'https://github.com/mbstevenson96/Adventures-of-Ace',
+    deploymentLink: 'https://adventures-of-ace.netlify.app/',
+  },
+  {
+    image: 'https://i.imgur.com/CG7jWJw.png',
+    title: 'Bourbon Experience',
+    description: "The Bourbon Experience is a community where you can track your personal collection, browse your friends' collections, and build your custom wish list. Use this space to connect with your friends and review their collections as well and build your community.",
+    repositoryLink: 'https://github.com/mbstevenson96/bourbon-experience',
+    deploymentLink: 'https://bourbon-experience.fly.dev/',
+  },
+  {
+    image: 'https://i.imgur.com/dMxNFXs.png',
+    title: 'LAMdb',
+    description: "It’s hard to keep track of vet records and invoices over the years, and most of us as pet owners rely solely on our email to do so. No matter if you have one fur-baby or five, that lack of organization makes managing our pet health history a challenging and daunting task!",
+    repositoryLink: 'https://github.com/shakestuffup73/lamdb-frontend',
+    deploymentLink: 'https://lamdb.netlify.app/',
+    contributors: 'Amanda Stern, Lily Howlader, Maddie Stevenson'
+  },
+]
