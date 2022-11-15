2070652faebbebdf453fdfeee21c3527b29feb30
mstevenson <mbstevenson96@gmail.com>
2022-11-14 21:45:10 -0500

    style project preview

--- src/components/ProjectPreview/ProejctPreview.jsx
+++ src/components/ProjectPreview/ProejctPreview.jsx
@@ -1,21 +1,23 @@
 import hyphenateWords from '../../utilities/hyphenateWords'
 import { Link } from 'react-router-dom'
+import styles from './ProjectPreview.module.css'
+
 
 const ProjectPreview = (props) => {
   const projectPath = hyphenateWords(props.title)
   return ( 
-    <>
+    <div className={styles.container}>
       <img 
         src={props.image} 
         alt={props.title}
       />
       <section>
-        <h2>{props.title}</h2>
+        <h2 className={styles.ProjectH1}>{props.title}</h2>
         <Link to={projectPath}>
           <button>Learn More About This Project</button>
         </Link>
       </section>
-    </>
+    </div>
   );
 }
 
