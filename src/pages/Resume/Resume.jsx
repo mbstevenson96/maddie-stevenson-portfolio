44201787acb6c8120d9d1a9e14b8a61b068f0741
mstevenson <mbstevenson96@gmail.com>
2022-11-14 21:33:24 -0500

    style resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,19 +1,20 @@
 import MaddieLamb from '../../assets/MaddieLamb.jpg'
-import ResumeIcon from '../../assets/ResumeIcon.jpg'
+import ResumeIcon from '../../assets/ResumeIcon.png'
+import styles from './Resume.module.css'
 
 
 const Resume = () => {
   return ( 
     <>
-      <div>
-        <img src={MaddieLamb} alt="Maddie hiking in Ireland"/>
+      <div className={styles.container}>
+        <img className={styles.ResumePhoto} src={MaddieLamb} alt="Maddie hiking in Ireland"/>
+        <h1 className={styles.ResumeH1}>Click the icon below to view my resume!</h1>
+        <a href='https://drive.google.com/file/d/1KKhO1pxO0iYsKNUYVPNkxZT9qOrH_1h5/view?usp=sharing' target="_blank" rel="noopener noreferrer">
+          <button>
+            <img className={styles.ResumeIcon} src={ResumeIcon} alt="resume icon"/>
+          </button>
+        </a>
       </div>
-      <h1>Click the icon below to view my resume!</h1>
-      <a href='https://drive.google.com/file/d/1Nop0vBCSzTG6x-19dIvkNj3SPp_JJ20u/view?usp=sharing' target="_blank" rel="noopener noreferrer">
-        <button>
-          <img src={ResumeIcon} alt="resume icon"/>
-        </button>
-      </a>
     </>
   );
 }
