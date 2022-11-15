44568434054c583b1700637f4dfb17b8311d5a83
mstevenson <mbstevenson96@gmail.com>
2022-11-14 21:54:38 -0500

    style project details page

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,23 +1,28 @@
 import findProject from '../../utilities/findProject'
 import { useParams } from "react-router-dom";
+import styles from './ProjectDetails.module.css'
 
 const ProjectDetails = () => {
   const { projectDetails } = useParams()
   const project = findProject(projectDetails)
 
   return (
-    <>
+    <div className={styles.container}>
       <img 
         src={project.image} 
         alt={project.title}
       />
       <section>
-        <h1>{project.title}</h1>
-        <p>{project.description}</p>
-        <a href={project.repositoryLink} target='_blank' rel='noreferrer'>GitHub Repo</a>
-        <a href={project.deploymentLink} target='_blank' rel='noreferrer'>Deployed Site</a>
+        <h1 className={styles.DetailsH1}>{project.title}</h1>
+        <p className={styles.DetailsP}>{project.description}</p>
+        <div className={styles.GithubContainer}>
+          <a href={project.repositoryLink} target='_blank' rel='noreferrer'>Click for GitHub Repo</a>
+        </div>
+        <div className={styles.DeployedContainer}>
+          <a href={project.deploymentLink} target='_blank' rel='noreferrer'> Click for Deployed Site</a>
+        </div>
       </section>
-    </>
+    </div>
   );
 }
 
