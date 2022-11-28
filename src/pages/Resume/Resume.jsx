aaff761a9d54bb610fd47713a5b26303b41e1df9
mstevenson <mbstevenson96@gmail.com>
2022-11-28 17:53:51 -0500

    update resume styling and add object to show resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,20 +1,28 @@
 import MaddieLamb from '../../assets/MaddieLamb.jpg'
 import ResumeIcon from '../../assets/ResumeIcon.png'
+import MaddieResume from '../../assets/MadeleineStevensonResume.pdf'
 import styles from './Resume.module.css'
 
 
 const Resume = () => {
   return ( 
     <>
-      <div className={styles.container}>
+      <main className={styles.container}>
         <img className={styles.ResumePhoto} src={MaddieLamb} alt="Maddie hiking in Ireland"/>
-        <h1 className={styles.ResumeH1}>Click the icon below to view my resume!</h1>
-        <a href='https://drive.google.com/file/d/1xF-7FlAZJMOMGW0i36hAJgGi-23vLk93/view?usp=sharing' target="_blank" rel="noopener noreferrer">
+          <object 
+            className={styles.ResumeObject}
+            type='application/pdf'
+            data={MaddieResume}
+            height="90%"
+            width="80%">
+          </object>
+          <h1 className={styles.ResumeH1}>Click the icon below to download my resume!</h1>
+          <a href='https://drive.google.com/file/d/1xF-7FlAZJMOMGW0i36hAJgGi-23vLk93/view?usp=sharing'>
           <button>
             <img className={styles.ResumeIcon} src={ResumeIcon} alt="resume icon"/>
           </button>
-        </a>
-      </div>
+          </a>
+      </main>
     </>
   );
 }
