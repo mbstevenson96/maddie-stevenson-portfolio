77a39c5fc0180f1d99bdf374e6a9b38f754ec44d
mstevenson <mbstevenson96@gmail.com>
2022-12-02 18:58:10 -0500

    fix spaceing on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,11 @@ const Home = () => {
     <>
       <div className={styles.container}>
         <img className={styles.HomePhoto} src={Maddie1} alt="Maddie in garden"/>
-        <h1 className={styles.HomeH1}>Maddie Stevenson | Software Engineer</h1>
+        <div className={styles.Info}>
+          <h1 className={styles.HomeH1}>Maddie Stevenson | Software Engineer</h1>
+          <p className={styles.HomeH1}>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
+          <p className={styles.HomeH1}>When I am not pursuing my passion of software engineering I am discovering new outdoor adventures, training with my dog Ace, reading, and exploring new travels.</p>
+        </div>
       </div>
     </>
   );
