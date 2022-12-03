03c6872e394d2343dc0a4232d84a6c4e28b7a86b
mstevenson <mbstevenson96@gmail.com>
2022-12-02 20:29:24 -0500

    update mobile styling on contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,15 +7,18 @@ const Contact = () => {
     <>
       <div className={styles.container}>
         <img className={styles.ContactPhoto} src={Snowboarding} alt="Maddie Snowboarding"/>
-        <h1 className={styles.ContactH1}>Contact Me!</h1>
-        <div className={styles.gmailContainer}>
-          <p className={styles.ContactP}>Email: mbstevenson96@gmail.com</p>
+        <div className={styles.Info}>
+          <h1 className={styles.ContactH1}>Contact Me!</h1>
+          <div className={styles.gmailContainer}>
+            <p className={styles.ContactP}>Email: mbstevenson96@gmail.com</p>
+          </div>
+          <div className={styles.githubContainer}>
+            <a className={styles.ContactA} href='https://github.com/mbstevenson96'> Click to View GitHub Profile</a>
+          </div>
+          <div className={styles.linkedinContainer}>
+            <a className={styles.ContactA} href='https://www.linkedin.com/in/mstevenson11/'>Click to View Linkedin Profile</a>
+
         </div>
-        <div className={styles.githubContainer}>
-          <a className={styles.ContactA} href='https://github.com/mbstevenson96' target="_blank" rel="noopener noreferrer"> Click to View GitHub Profile</a>
-        </div>
-        <div className={styles.linkedinContainer}>
-          <a className={styles.ContactA} href='https://www.linkedin.com/in/mstevenson11/' target="_blank" rel="noopener noreferrer">Click to View Linkedin Profile</a>
         </div>
       </div>
     </>
