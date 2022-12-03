d92be29cb701bf4ed46c09462fb1e0f92c14ee41
mstevenson <mbstevenson96@gmail.com>
2022-12-02 20:08:06 -0500

    update home page for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from './NavBar.module.css'
 
-
 const NavBar = () => {
+
   return ( 
-    // <nav className={styles.nav}>
-      <div className={styles.container}>
-        <Link to='/' className={styles.navLink}>Home</Link>
-        <Link to='/about' className={styles.navLink}>About</Link>
-        <Link to='/contact' className={styles.navLink}>Contact</Link>
-        <Link to='/projects' className={styles.navLink}>Projects</Link>
-        <Link to='/resume' className={styles.navLink}>Resume</Link>
-      </div>
-    // </nav>
+    <div className={styles.container}>
+      <Link to='/' className={styles.navLink}>Home</Link>
+      <Link to='/about' className={styles.navLink}>About</Link>
+      <Link to='/contact' className={styles.navLink}>Contact</Link>
+      <Link to='/projects' className={styles.navLink}>Projects</Link>
+      <Link to='/resume' className={styles.navLink}>Resume</Link>
+    </div>
   );
 }
 
