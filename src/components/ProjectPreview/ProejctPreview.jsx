eb10e58f6b1b76efa379fb6950aae1b826a83d1e
mstevenson <mbstevenson96@gmail.com>
2022-12-19 15:08:37 -0500

    update responsive styling for projects overview page

--- src/components/ProjectPreview/ProejctPreview.jsx
+++ src/components/ProjectPreview/ProejctPreview.jsx
@@ -11,7 +11,7 @@ const ProjectPreview = (props) => {
         src={props.image} 
         alt={props.title}
       />
-      <section>
+      <section className={styles.infoContainer}>
         <h2 className={styles.ProjectH1}>{props.title}</h2>
         <Link to={projectPath}>
           <button className={styles.ProjectButton}>Learn More About This Project</button>
