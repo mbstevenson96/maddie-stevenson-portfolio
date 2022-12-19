13173e6046de44143c819b71fc44f051f535f599
mstevenson <mbstevenson96@gmail.com>
2022-12-19 15:18:15 -0500

    update mobile styling and repsonsive styling for resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,5 +1,4 @@
 import MaddieLamb from '../../assets/MaddieLamb.jpg'
-import ResumeIcon from '../../assets/ResumeIcon.png'
 import MaddieResume from '../../assets/MadeleineStevensonResume.pdf'
 import styles from './Resume.module.css'
 
@@ -9,19 +8,13 @@ const Resume = () => {
     <>
       <main className={styles.container}>
         <img className={styles.ResumePhoto} src={MaddieLamb} alt="Maddie hiking in Ireland"/>
-          <object 
-            className={styles.ResumeObject}
-            type='application/pdf'
-            data={MaddieResume}
-            height="90%"
-            width="80%">
-          </object>
-          <h1 className={styles.ResumeH1}>Click the icon below to download my resume!</h1>
-          <a href='https://drive.google.com/file/d/1xF-7FlAZJMOMGW0i36hAJgGi-23vLk93/view?usp=sharing'>
-          <button>
-            <img className={styles.ResumeIcon} src={ResumeIcon} alt="resume icon"/>
-          </button>
-          </a>
+        <object 
+          className={styles.ResumeObject}
+          type='application/pdf'
+          data={MaddieResume}
+          height="90%"
+          width="80%">
+        </object>
       </main>
     </>
   );
