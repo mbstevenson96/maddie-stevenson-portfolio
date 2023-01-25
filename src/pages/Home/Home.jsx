168d03493414526f3b9e63da1cafc0aa6fa5b6cb
mstevenson <mbstevenson96@gmail.com>
2023-01-25 14:31:16 -0600

    stub up tech stack section for icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Maddie1 from '../../assets/Maddie1.JPG'
 import styles from './Home.module.css'
 
-const Home = () => {
+const Home = (props) => {
   return ( 
     <>
       <div className={styles.container}>
@@ -10,6 +10,13 @@ const Home = () => {
           <h1 className={styles.HomeH1}>Maddie Stevenson | Software Engineer</h1>
           <p className={styles.HomeH1}>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
           <p className={styles.HomeH1}>When I am not pursuing my passion of software engineering I am discovering new outdoor adventures, training with my dog Ace, reading, and exploring new travels.</p>
+        </div>
+        <div>
+          <h1>Tech Stack</h1>
+          <img
+            src={props.language} 
+            alt={props.title}
+          />
         </div>
       </div>
     </>
