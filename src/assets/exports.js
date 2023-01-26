48b6795143623de1773c780a5c48fb4923a78c1c
mstevenson <mbstevenson96@gmail.com>
2023-01-26 02:34:46 -0600

    stub up adding in tech stacks

---
+++ src/assets/exports.js
@@ -0,0 +1,17 @@
+import React from './TechStacks/react.png'
+import Javascript from './TechStacks/javascript.png'
+
+const ReactIcon = {
+  src: React,
+  alt: 'React icon'
+}
+
+const JavascriptIcon = {
+  src: Javascript,
+  alt: 'Javascript Icon'
+}
+
+export {
+  ReactIcon,
+  JavascriptIcon
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,24 @@
 import Maddie1 from '../../assets/Maddie1.JPG'
 import styles from './Home.module.css'
+import {
+  ReactIcon,
+  JavascriptIcon,
+} from '../../assets/exports.js'
 
-const Home = (props) => {
+const Home = () => {
   return ( 
     <>
       <div className={styles.container}>
         <img className={styles.HomePhoto} src={Maddie1} alt="Maddie in garden"/>
         <div className={styles.Info}>
           <h1 className={styles.HomeH1}>Maddie Stevenson | Software Engineer</h1>
-          <p className={styles.HomeH1}>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
+          <p className={styles.HomeH1}>Inspired by the creation of Web3 and browser access to the metaverse, my experience with working in this industry has led to my passion for software development. My passion is driven by wanting to build  communities where geography isn’t an issue. Bringing others together can create a world where unity is universal.</p>
           <p className={styles.HomeH1}>When I am not pursuing my passion of software engineering I am discovering new outdoor adventures, training with my dog Ace, reading, and exploring new travels.</p>
         </div>
         <div>
-          <h1>Tech Stack</h1>
-          <img
-            src={props.language} 
-            alt={props.title}
-          />
+          <h1 className={styles.HomeH1}>Tech Stack</h1>
+          <img src={ReactIcon.src} alt={ReactIcon.alt} />
+          <img src={JavascriptIcon.src} alt={JavascriptIcon.alt} />
         </div>
       </div>
     </>
