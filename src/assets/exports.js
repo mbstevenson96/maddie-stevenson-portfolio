dff678ea49eaff70475adc2e8bac54e5232dcb05
mstevenson <mbstevenson96@gmail.com>
2023-01-26 03:57:07 -0600

    add tech icons to exports file

--- src/assets/exports.js
+++ src/assets/exports.js
@@ -1,5 +1,20 @@
-import React from './TechStacks/react.png'
-import Javascript from './TechStacks/javascript.png'
+import React from './TechStacks/react.svg'
+import Javascript from './TechStacks/javascript.svg'
+import Html from './TechStacks/html.svg'
+import Css from './TechStacks/css.svg'
+import Python from './TechStacks/python.svg'
+import Express from './TechStacks/express.svg'
+import Django from './TechStacks/django.svg'
+import Bootstrap from './TechStacks/bootstrap.svg'
+import Docker from './TechStacks/docker.svg'
+import VsCode from './TechStacks/vscode.svg'
+import Netlify from './TechStacks/netlify.svg'
+import Nodejs from './TechStacks/nodejs.svg'
+import Npm from './TechStacks/npm.svg'
+import Nodemon from './TechStacks/nodemon.svg'
+import Postgresql from './TechStacks/postgresql.svg'
+import Mongodb from './TechStacks/mongodb.svg'
+import Postman from './TechStacks/postman.svg'
 
 const ReactIcon = {
   src: React,
@@ -11,7 +26,98 @@ const JavascriptIcon = {
   alt: 'Javascript Icon'
 }
 
+const HtmlIcon = {
+  src: Html,
+  alt: 'Html Icon'
+}
+
+const CssIcon = {
+  src: Css,
+  alt: 'CSS Icon'
+}
+
+const PythonIcon = {
+  src: Python,
+  alt: 'Python Icon'
+}
+
+const ExpressIcon = {
+  src: Express,
+  alt: 'Express Icon'
+}
+
+const DjangoIcon = {
+  src: Django,
+  alt: 'Django Icon'
+}
+
+const BootstrapIcon = {
+  src: Bootstrap,
+  alt: 'Bootstrap Icon'
+}
+
+const DockerIcon = {
+  src: Docker,
+  alt: 'Docker Icon'
+}
+
+const VsCodeIcon = {
+  src: VsCode,
+  alt: 'VS Code Icon'
+}
+
+const NetlifyIcon = {
+  src: Netlify,
+  alt: 'Netlify Icon'
+}
+
+const NodejsIcon = {
+  src: Nodejs,
+  alt: 'Node.js Icon'
+}
+
+const NpmIcon = {
+  src: Npm,
+  alt: 'NPM Icon'
+}
+
+const NodemonIcon = {
+  src: Nodemon,
+  alt: 'Nodemon Icon'
+}
+
+const PostgresqlIcon = {
+  src: Postgresql,
+  alt: 'PostgreSQL Icon'
+}
+
+const MongodbIcon = {
+  src: Mongodb,
+  alt: 'MongoDB Icon'
+}
+
+const PostmanIcon = {
+  src: Postman,
+  alt: 'Postman Icon'
+}
+
+
 export {
   ReactIcon,
-  JavascriptIcon
+  JavascriptIcon,
+  HtmlIcon,
+  CssIcon,
+  PythonIcon,
+  ExpressIcon,
+  DjangoIcon,
+  BootstrapIcon,
+  DockerIcon,
+  VsCodeIcon,
+  NetlifyIcon,
+  NodejsIcon,
+  NpmIcon,
+  NodemonIcon,
+  PostgresqlIcon,
+  MongodbIcon,
+  PostmanIcon,
 }
